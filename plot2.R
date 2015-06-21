d34b4540ad1a67e1ee54397c9c089bba66622e44
Sudhir <sudhirchowdhary@gmail.com>
2015-06-21 16:24:28 -0500

    Changes to the Code/Images
    Changes to the Code/Images

--- plot2.R
+++ plot2.R
@@ -6,12 +6,12 @@ SCC <- readRDS("Source_Classification_Code.rds")
 
 bmoreCond <- NEI[NEI$fips=="24510",]
 
-aggTotalsBaltimore <- aggregate(Emissions ~ year, bmoreCond,sum)
+aggTotalsBmore <- aggregate(Emissions ~ year, bmoreCond,sum)
 
 
 barplot(
-        aggTotalsBaltimore$Emissions,
-        names.arg=aggTotalsBaltimore$year,
+        aggTotalsBmore$Emissions,
+        names.arg=aggTotalsBmore$year,
         xlab="Year",
         ylab="PM2.5 (Tons)",
         main="Total PM2.5 Emissions From All Baltimore City Sources (between years 1999-2008)"
